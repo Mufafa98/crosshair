2ab80328a1222d0728620aedec1915868a10c76f
Mufafa98 <marian.parlea2@gmail.com>
2026-07-13 20:37:29 +0300

    Added config file

--- src/crosshair.rs
+++ src/crosshair.rs
@@ -1,6 +1,10 @@
 use gtk4::cairo::Context;
+use serde::{Deserialize, Serialize};
+use std::{env, fs, path::PathBuf};
+use toml;
 
-struct CrosshairConfig {
+#[derive(Clone, Deserialize, Serialize)]
+pub struct CrosshairConfig {
     gap: f64,
     len: f64,
     width: f64,
@@ -13,6 +17,34 @@ impl Default for CrosshairConfig {
             len: 15.0,
             width: 2.0,
         }
+    }
+}
+
+impl CrosshairConfig {
+    pub fn load_from_file() -> Self {
+        if cfg!(target_os = "windows") {
+            return Self::default();
+        } else if cfg!(target_os = "linux") {
+            let home = env::var("HOME").expect("HOME directory not found");
+            let config_dir = PathBuf::from(home).join(".config").join("fafa_cross");
+            let config_path = config_dir.join("config.toml");
+
+            if !config_path.exists() {
+                fs::create_dir_all(&config_dir).unwrap();
+                let default_config = Self::default();
+                let toml_string = toml::to_string(&default_config).unwrap();
+                fs::write(&config_path, toml_string).unwrap();
+
+                return default_config;
+            }
+
+            // Read existing file
+            return fs::read_to_string(&config_path)
+                .ok()
+                .and_then(|content| toml::from_str(&content).ok())
+                .unwrap_or_default();
+        }
+        return Self::default();
     }
 }
 
@@ -84,8 +116,7 @@ fn draw_line(
     cr.stroke().unwrap();
 }
 
-pub fn draw_crosshair(cr: &Context, center: &Point) {
-    let config = CrosshairConfig::default();
+pub fn draw_crosshair(cr: &Context, center: &Point, config: &CrosshairConfig) {
     let color = Color {
         r: 1.0,
         g: 0.0,
